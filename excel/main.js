c3fe2d87dd324a78af28bdd7532ab50c216075a9
unknown <pengshudream@gmail.com>
2020-03-20 14:32:10 +0800

    create

--- excel/main.js
+++ excel/main.js
@@ -1,3 +1,8 @@
 const xlsx = require("node-xlsx").default;
+const fs = require("fs");
+
+
 let list = xlsx.parse("./excel/data.xlsx");
-console.log(list[0].data);
+fs.writeFile("./excel/json/data.json",JSON.stringify(list),(res)=>{
+    console.log(`writeFile data.json success ${res}`);
+})
