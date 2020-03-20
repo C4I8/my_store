bb404fc6fc5c59ac0c4e079b2df8fd20b184c06c
unknown <pengshudream@gmail.com>
2020-03-20 18:07:53 +0800

    syn

--- excel/main.js
+++ excel/main.js
@@ -1,7 +1,6 @@
 const xlsx = require("node-xlsx").default;
 const fs = require("fs");
 
-
 let list = xlsx.parse("./excel/data.xlsx");
 fs.writeFile("./excel/json/data.json",JSON.stringify(list),(res)=>{
     console.log(`writeFile data.json success ${res}`);
