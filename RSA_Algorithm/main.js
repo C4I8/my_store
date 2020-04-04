279af9abd414790a178cc1f371d82acfa4f8555e
C4I8 <pengshudream@gmail.com>
2020-04-04 20:55:43 +0800

    syn

--- RSA_Algorithm/main.js
+++ RSA_Algorithm/main.js
@@ -1,4 +1,25 @@
+let fs = require("fs");
 /**大质数发生器 */
-function primeGenerate(){
-
+function primeGenerate(N){
+    let primes = [];
+    for(let i = 2;i<=N;i++){
+        let prime = true;
+        for(let j = 2;j<i;j++){
+            if(i % j == 0){
+                prime = false;
+                break;
+            }
+        }
+        if(prime){
+            primes.push(i);
+        }
+    }
+    fs.writeFile("./json/prime.json",primes.toString(),()=>{
+        console.log(`prime.json write success`);
+    });
+    return primes;
 }
+let startTime = Date.now();
+primeGenerate(99999999999);
+let endTime = Date.now();
+console.log(`duration: ${endTime - startTime}`);
